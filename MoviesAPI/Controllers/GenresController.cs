0e43ffea0cd3c36b37faf2fdde6aa7358762c054
Med Salmen Saadi <medsalmen.saadi@esprit.tn>
2023-12-12 08:28:08 -0500

    Add DELETE Genres Endpoint

--- MoviesAPI/Controllers/GenresController.cs
+++ MoviesAPI/Controllers/GenresController.cs
@@ -50,5 +50,19 @@ namespace MoviesAPI.Controllers
 
             return Ok(genre);
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var genre = await _context.Genres.SingleOrDefaultAsync(g => g.Id == id);
+
+            if (genre == null)
+                return NotFound($"No genre was found with ID: {id}");
+
+            _context.Remove(genre);
+            _context.SaveChanges();
+
+            return Ok(genre);
+        }
     }
 }
