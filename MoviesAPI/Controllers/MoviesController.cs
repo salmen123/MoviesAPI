26f1f1ab03b0be2540bebe5331fb292f294de95d
Med Salmen Saadi <medsalmen.saadi@esprit.tn>
2023-12-13 09:24:13 -0500

    Add [POST] Create Movie Endpoint

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Dtos;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        private List<string> _allowedExtenstions = new List<string> { ".jpg", ".png" };
+        private long _maxAllowedPosterSize = 1048576;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromForm] MovieDto dto)
+        {
+            if (!_allowedExtenstions.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+                return BadRequest("Only .jpg and .png images are allowed!");
+
+            if (dto.Poster.Length > _maxAllowedPosterSize)
+                return BadRequest("Max allowed size for poster is 1MB!");
+
+            var isValidGenre = await _context.Genres.AnyAsync(g => g.Id == dto.GenreId);
+
+            if (!isValidGenre)
+                return BadRequest("Invalid genere ID!");
+
+            using var dataStream = new MemoryStream();
+
+            await dto.Poster.CopyToAsync(dataStream);
+
+            var movie = new Movie
+            {
+                Title = dto.Title,
+                Year = dto.Year,
+                Rate = dto.Rate,
+                Storeline = dto.Storeline,
+                Poster = dataStream.ToArray(),
+                GenreId = dto.GenreId
+            };
+
+            await _context.AddAsync(movie);
+            _context.SaveChanges();
+
+            return Ok(movie);
+        }
+    }
+}
